13b9535fa12e19334700f85c9b67b40af372160d
Gustavo Azambuja <hola@gazambuja.com>
2010-06-08 16:15:05 -0300

    Modify README and mysql.php

--- inc/mysql.php
+++ inc/mysql.php
@@ -1,10 +1,6 @@
 <?
-//Configuracion de la conexion a base de datos
-$bd_host = "localhost"; 
-$bd_usuario = "username"; 
-$bd_password = "password"; 
 $con = mysql_connect($bd_host, $bd_usuario, $bd_password); 
-mysql_select_db("billing", $con);
+mysql_select_db($bd_database, $con);
 
 mysql_query("SET NAMES utf8");
 ?>
